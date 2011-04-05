f9de6a74080f08857a721ec182088fc21720de01
Michael Kuhinica <taverneiro@gmail.com>
2011-04-05 00:50:55 -0300

    COM UM SOPRO BINARIO DIVINO O PLAYER VEM A VIDA, ANDANDO AQUI E ACOLA

--- BaconEatingFatZombies/BaconEatingFatZombies/Game1.cs
+++ BaconEatingFatZombies/BaconEatingFatZombies/Game1.cs
@@ -1,5 +1,5 @@
 using System;using System.Collections.Generic;using System.Linq;using Microsoft.Xna.Framework;using Microsoft.Xna.Framework.Audio;using Microsoft.Xna.Framework.Content;using Microsoft.Xna.Framework.GamerServices;using Microsoft.Xna.Framework.Graphics;using Microsoft.Xna.Framework.Input;using Microsoft.Xna.Framework.Media;using Microsoft.Xna.Framework.Net;using Microsoft.Xna.Framework.Storage;namespace BaconEatingFatZombies{    public class Game1 : Microsoft.Xna.Framework.Game    {        private GraphicsDeviceManager graphics;        private SpriteBatch spriteBatch;        private Texture2D background;        private List<zombie> listaZumbis = new List<zombie>();        private List<bacon> listaBacon = new List<bacon>();
-
+        private player player;
 
         //private List<zombie> listaPixels = new List<zombie>();
         private List<bullet> listaBalas = new List<bullet>();
@@ -7,7 +7,11 @@ using System;using System.Collections.Generic;using System.Linq;using Microso
 
 
 
-
+            player = new player(Content.Load<Texture2D>("link"),
+                    new Vector2(centro.X+50f, centro.Y),
+                    new Vector2(34f, 42f),
+                    new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight)
+                );
 
             listaBacon.Add(new bacon(Content.Load<Texture2D>("bacon_peq"),
                         new Vector2(centro.X, centro.Y - 15),
@@ -25,7 +29,7 @@ using System;using System.Collections.Generic;using System.Linq;using Microso
                         new Vector2(centro.X + 15, centro.Y),
                         new Vector2(62f, 86f),
                         graphics.PreferredBackBufferWidth,
-                        graphics.PreferredBackBufferHeight));            for (int i = 0; i < 200; i++)            {                //Obtendo uma posicao inicial aleatoria                initialPosition = this.GetRandomInitialLocation(zombie.larguraTextura, zombie.alturaTextura);                //Centralizando a posicao do bicho (descontando o tamanho da imagem)                initialPosition = new Vector2(initialPosition.X, initialPosition.Y);                Texture2D texture = Content.Load<Texture2D>( zombie.GetSprite(initialPosition , this.matrixHigh ) );                listaZumbis.Add( new zombie(texture, initialPosition, new Vector2(62f, 86f), graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight, centro) );            }
+                        graphics.PreferredBackBufferHeight));            for (int i = 0; i < 20; i++)            {                //Obtendo uma posicao inicial aleatoria                initialPosition = this.GetRandomInitialLocation(zombie.larguraTextura, zombie.alturaTextura);                //Centralizando a posicao do bicho (descontando o tamanho da imagem)                initialPosition = new Vector2(initialPosition.X, initialPosition.Y);                Texture2D texture = Content.Load<Texture2D>( zombie.GetSprite(initialPosition , this.matrixHigh ) );                listaZumbis.Add( new zombie(texture, initialPosition, new Vector2(62f, 86f), graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight, centro) );            }
 
 
 
@@ -45,8 +49,9 @@ using System;using System.Collections.Generic;using System.Linq;using Microso
             foreach (bullet sprite in listaBalas)
             {
                 sprite.texture.Dispose();
-            }            spriteBatch.Dispose();        }        /// <summary>        /// Allows the game to run logic such as updating the world,        /// checking for collisions, gathering input, and playing audio.        /// </summary>        /// <param name="gameTime">Provides a snapshot of timing values.</param>        protected override void Update(GameTime gameTime)        {            // Allows the game to exit            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)                this.Exit();            KeyboardState keyboard = Keyboard.GetState();            //            if (keyboard.IsKeyDown(Keys.Right))            //                sprite1.MoveDireita();
+            }            spriteBatch.Dispose();        }        /// <summary>        /// Allows the game to run logic such as updating the world,        /// checking for collisions, gathering input, and playing audio.        /// </summary>        /// <param name="gameTime">Provides a snapshot of timing values.</param>        protected override void Update(GameTime gameTime)        {            // Allows the game to exit            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)                this.Exit();            KeyboardState keyboard = Keyboard.GetState();
 
+            player.AtualizaPosicao(keyboard);
 
 
 
@@ -89,4 +94,6 @@ using System;using System.Collections.Generic;using System.Linq;using Microso
             foreach (bacon sprite in listaBacon)
             {
                 sprite.Draw(spriteBatch);
-            }            //foreach (bullet sprite in listaBalas)            //{            //    sprite.Draw(spriteBatch);            //}            spriteBatch.End();            base.Draw(gameTime);        }        public Vector2 GetRandomInitialLocation( int larguraXImagem, int alturaYImagem )        {            int y = 0;            int x = 0;            y = random.Next(matrixLow, matrixHigh);            if (y > 0 && y < 501)            {                int lado = random.Next(2);  // exclusive upperBound                if (lado == 0)                {                    x = random.Next(matrixLow , 0 );                }                else                {                    x = random.Next(500 , matrixHigh );                }            }            else            {                x = random.Next(matrixLow, matrixHigh);            }            Console.WriteLine("x {0} y {1} ", x, y);            return new Vector2(x, y);        }    }}
+            }            //foreach (bullet sprite in listaBalas)            //{            //    sprite.Draw(spriteBatch);            //}
+
+            player.Draw(spriteBatch);            spriteBatch.End();            base.Draw(gameTime);        }        public Vector2 GetRandomInitialLocation( int larguraXImagem, int alturaYImagem )        {            int y = 0;            int x = 0;            y = random.Next(matrixLow, matrixHigh);            if (y > 0 && y < 501)            {                int lado = random.Next(2);  // exclusive upperBound                if (lado == 0)                {                    x = random.Next(matrixLow , 0 );                }                else                {                    x = random.Next(500 , matrixHigh );                }            }            else            {                x = random.Next(matrixLow, matrixHigh);            }            Console.WriteLine("x {0} y {1} ", x, y);            return new Vector2(x, y);        }    }}
