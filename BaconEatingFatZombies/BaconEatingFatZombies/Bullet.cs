d3d9f2df5fe75311f1c21ff75b7c86428b18df09
Fernando Pazin <pazinfernando@gmail.com>
2011-04-05 19:56:50 -0700

    Esse sera meu primeiro commit sem graca....

--- BaconEatingFatZombies/BaconEatingFatZombies/Bullet.cs
+++ BaconEatingFatZombies/BaconEatingFatZombies/Bullet.cs
@@ -1,47 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using Microsoft.Xna.Framework;using Microsoft.Xna.Framework.Graphics;namespace BaconEatingFatZombies{    class bullet : baseSprite    {        public bool colidiuZumbi { get; set; }        public bullet(Texture2D newTexture, Vector2 newPosition, Vector2 newSize, Vector2 newScreensize, Vector2 newDirection)        {            texture = newTexture;            position = newPosition;            InitialPosition = newPosition;            size = newSize;            screensize = newScreensize;            destination = newDirection;            colidiuZumbi = false;            determinaVelocidade();            calculaEquacaoReta();        }        //nomes dos arquivos das bullets usando coordenadas cartesianas ( Norte, Sul, blablabla )        public const string bullet_N = "bullet-N";        public const string bullet_S = "bullet-S";        public const string bullet_E = "bullet-E";        public const string bullet_W = "bullet-W";        public const string bullet_SW = "bullet-SW";        public const string bullet_SE = "bullet-SE";        public const string bullet_NW = "bullet-NW";        public const string bullet_NE = "bullet-NE";                public const int alturaTextura = 44;        public const int larguraTextura = 50;        public void AtualizaPosicao()        {            if (!colidiuZumbi)                position = proximaPosicaoPelaEquacaoReta();        }
 
-namespace BaconEatingFatZombies
-{
-    class bullet : baseSprite
-    {
+        //Baseado na posicao em que o player esta olhando
+        //public static string GetSprite(Vector2 InitialPosition, Vector2 destination, int maxMatrix)
+        //{
 
-        public bool colidiuZumbi { get; set; }
-
-        public bullet(Texture2D newTexture, Vector2 newPosition, Vector2 newSize, Vector2 newScreensize, Vector2 newDirection)
-        {
-            texture = newTexture;
-            position = newPosition;
-            InitialPosition = newPosition;
-            size = newSize;
-            screensize = newScreensize;
-            destination = newDirection;
-
-            colidiuZumbi = false;
-
-            determinaVelocidade();
-            calculaEquacaoReta();        }
-
-        //nomes dos arquivos das bullets usando coordenadas cartesianas ( Norte, Sul, blablabla )
-        public const string bullet_N = "bullet-N";
-        public const string bullet_S = "bullet-S";
-        public const string bullet_E = "bullet-E";
-        public const string bullet_W = "bullet-W";
-        public const string bullet_SW = "bullet-SW";
-        public const string bullet_SE = "bullet-SE";
-        public const string bullet_NW = "bullet-NW";
-        public const string bullet_NE = "bullet-NE";
-        
-        public const int alturaTextura = 44;
-        public const int larguraTextura = 50;
-
-        public void AtualizaPosicao()
-        {
-            if (!colidiuZumbi)
-                position = proximaPosicaoPelaEquacaoReta();        }
-    }
-}
+        //}    }}
--- BaconEatingFatZombies/BaconEatingFatZombies/Player.cs
+++ BaconEatingFatZombies/BaconEatingFatZombies/Player.cs
@@ -1,58 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-namespace BaconEatingFatZombies
-{
-    class player : baseSprite
-    {
-        private float velocidade = 3f;
-        public player(Texture2D newTexture, Vector2 newPosition, Vector2 newSize, Vector2 newScreensize)        {            texture = newTexture;            position = newPosition;
-            InitialPosition = position;            size = newSize;
-            screensize = newScreensize;        }
-
-        public void MoveLeste()
-        {
-            MoveTo(new Vector2(position.X + velocidade, position.Y));
-        }
-
-        public void MoveOeste()
-        {
-            MoveTo(new Vector2(position.X - velocidade, position.Y));
-        }
-
-        public void MoveSul()
-        {
-            MoveTo(new Vector2(position.X, position.Y + velocidade));
-        }
-
-        public void MoveNorte()
-        {
-            MoveTo(new Vector2(position.X, position.Y - velocidade));
-        }
-
-        private void MoveTo(Vector2 newPosition) {
-            if (newPosition.X > size.X/2 && newPosition.X < screensize.X - (size.X/2)
-                && newPosition.Y > size.X/2 && newPosition.Y < screensize.Y - (size.X / 2))
-            {
-                position = newPosition;
-            }
-        }
-
-        public void AtualizaPosicao(KeyboardState keyboard)
-        {
-            if (keyboard.IsKeyDown(Keys.Right))
-                MoveLeste();
-            if (keyboard.IsKeyDown(Keys.Left))
-                MoveOeste();
-            if (keyboard.IsKeyDown(Keys.Up))
-                MoveNorte();
-            if (keyboard.IsKeyDown(Keys.Down))
-                MoveSul();
-        }
-    }
-}
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using Microsoft.Xna.Framework;using Microsoft.Xna.Framework.Graphics;using Microsoft.Xna.Framework.Input;namespace BaconEatingFatZombies{    class player : baseSprite    {        private float velocidade = 3f;        public player(Texture2D newTexture, Vector2 newPosition, Vector2 newSize, Vector2 newScreensize)        {            texture = newTexture;            position = newPosition;            InitialPosition = position;            size = newSize;            screensize = newScreensize;        }        public void MoveLeste()        {            MoveTo(new Vector2(position.X + velocidade, position.Y));        }        public void MoveOeste()        {            MoveTo(new Vector2(position.X - velocidade, position.Y));        }        public void MoveSul()        {            MoveTo(new Vector2(position.X, position.Y + velocidade));        }        public void MoveNorte()        {            MoveTo(new Vector2(position.X, position.Y - velocidade));        }        private void MoveTo(Vector2 newPosition) {            if (newPosition.X > size.X/2 && newPosition.X < screensize.X - (size.X/2)                && newPosition.Y > size.X/2 && newPosition.Y < screensize.Y - (size.X / 2))            {                position = newPosition;            }        }        public void AtualizaPosicao(KeyboardState keyboard)        {            if (keyboard.IsKeyDown(Keys.Right))                MoveLeste();            if (keyboard.IsKeyDown(Keys.Left))                MoveOeste();            if (keyboard.IsKeyDown(Keys.Up))                MoveNorte();            if (keyboard.IsKeyDown(Keys.Down))                MoveSul();        }    }}
